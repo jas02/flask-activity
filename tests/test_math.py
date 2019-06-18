783f06899baeed5cc7cb770c328db9c056ff9d12
Lumír Jasiok <lumir.jasiok@tieto.com>
2019-06-18 13:40:44 +0200

    Added basic test

---
+++ tests/test_math.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#
+#  Copyright (C) 2019 Tieto Czech s.r.o.
+#  Lumir Jasiok
+#  lumir.jasiok@tieto.com
+#  http://www.tieto.com
+#
+#
+#
+
+import math
+
+def test_sqrt():
+   num = 25
+   assert math.sqrt(num) == 5
+
